32828fb3c7797664e3c702491c5179da6721caba
Neil Brooks <neil.brooks87@gmail.com>
2026-05-09 21:01:51 +0100

    Mobile

--- src/Components/Lander/Index.js
+++ src/Components/Lander/Index.js
@@ -36,16 +36,24 @@ export default function Lander() {
             </div>
             <div className='itemRow'>
                 <h4>Selected Plaicin worlds<span className='help'>?</span></h4>
-                <img src={abbotts} alt="croydon" className="portraitImage" />
-                <img src={pleasance} alt="hungry grass" className="portraitImage" />
-                <img src={trilogy} alt="dublin" className="portraitImage" />
+                <a href="https://plaicin.com/worlds/dc7cc93a-5b4e-4f7f-9b28-1f49c6be9fe8?sharer_id=30746037-8c3d-461c-8a92-5fa4120e660f&share_channel=copy_link" target="_blank" rel="noopener noreferrer">
+                    <img src={abbotts} alt="croydon" className="portraitImage" />
+                </a>
+                <a href="https://plaicin.com/worlds/f546c4e3-811c-461b-9a02-9d52b8b38234?sharer_id=30746037-8c3d-461c-8a92-5fa4120e660f&share_channel=copy_link" target="_blank" rel="noopener noreferrer">
+                    <img src={pleasance} alt="hungry grass" className="portraitImage" />
+                </a>
+                <a href="https://plaicin.com/worlds/dc7cc93a-5b4e-4f7f-9b28-1f49c6be9fe8?sharer_id=30746037-8c3d-461c-8a92-5fa4120e660f&share_channel=copy_link" target="_blank" rel="noopener noreferrer">
+                    <img src={trilogy} alt="dublin" className="portraitImage" />
+                </a>
                 <h5 style={{marginTop: '3px'}}>See all Plaicin worlds</h5>
             </div>
             <div className="footer">
                 <h4>Written and directed by me</h4>
                 <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center',alignItems: 'center', gap: '10px', marginTop: '-20px'}}>
                     <h4>Powered by</h4>
-                    <img src={twizlLogo} alt="twizl" className="twizlLogo" />
+                    <a href="https://twizl.com/" target="_blank" rel="noopener noreferrer">
+                        <img src={twizlLogo} alt="twizl" className="twizlLogo" />
+                    </a>
                 </div>
             </div>
         </div>)}
